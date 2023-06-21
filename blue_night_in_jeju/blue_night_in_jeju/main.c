4d0df6059c1f06564ebe62e7f397cfc39e61e4d1
seoyeon kim <w2222@e-mirim.hs.kr>
2023-06-21 16:53:00 +0900

    Add files via upload

---
+++ blue_night_in_jeju/blue_night_in_jeju/main.c
@@ -0,0 +1,53 @@
+<<<<<<< HEAD:main.c
+﻿#include "main.h"
+#include <windows.h>
+#pragma comment (lib, "winmm.lib")
+#include <mmsystem.h>;
+
+int main() {
+	init();
+	while (1) {
+
+		PlaySound(TEXT("gamemusic.wav"), NULL, SND_ASYNC | SND_LOOP);
+
+		titleDraw();
+		int menuCode = menuDraw();
+		if (menuCode == 0) {
+			start();
+
+		}
+		else if (menuCode == 1) {
+			infoDraw();
+		}
+		system("cls");
+	}
+
+	return 0;
+}
+=======
+#include "main.h"
+
+
+int main() {
+	init();
+	while (1) {
+		
+		PlaySound(TEXT("gamemusic.wav") , NULL, SND_ASYNC | SND_LOOP);	//반복재생
+		
+		titleDraw();
+		int menuCode = menuDraw();
+		if (menuCode == 0) {
+			// °ÔÀÓ½ÃÀÛ
+			start();
+
+		}
+		else if (menuCode == 1) {
+			// °ÔÀÓÁ¤º¸
+			infoDraw();
+		}
+		system("cls");
+	}
+
+	return 0;
+}
+>>>>>>> 5984a707824718d4751ea760a076aea13a9b6f43:blue_night_in_jeju/main.c
---
+++ blue_night_in_jeju/blue_night_in_jeju/main.h
@@ -0,0 +1,33 @@
+<<<<<<< HEAD:main.h
+#ifndef __STDIO_H__
+#define __STDIO_H__
+#include <stdio.h>
+
+#endif
+
+#ifndef __WINDOW_H__
+#define __WINDOW_H__
+
+#include <windows.h>
+#endif
+
+
+
+=======
+#ifndef __STDIO_H__
+#define __STDIO_H__
+
+#include <stdio.h>
+
+#endif
+
+#ifndef __WINDOW_H__
+#define __WINDOW_H__
+
+#include <windows.h>
+#endif
+
+
+
+
+>>>>>>> 5984a707824718d4751ea760a076aea13a9b6f43:blue_night_in_jeju/main.h
